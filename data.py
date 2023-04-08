0fcc1521bbf3081d1413e66af69ef675165508f8
s43c <124310778+s43c@users.noreply.github.com>
2023-04-08 05:52:05 +0400

    Update data.py

--- data.py
+++ data.py
@@ -2,6 +2,6 @@
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
 
-api_id = 123456  # Your ID here | 请替换为你的id
-api_hash = 'Your Token Here | 请填写API_Hash'
-delay = 15  # Second
+api_id = 1007498  # Your ID here | 请替换为你的id
+api_hash = '57c6f3c72c2f21676d53be2e1deab3aa'
+delay = 10  # Second
